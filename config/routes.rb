9b44aba499a13c0d7f46de0320af65f40282ea44
Michael R <54217603+Michael-R-R@users.noreply.github.com>
2023-08-02 13:05:13 -0700

    Added simple login/logout;

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
   get("/experience", to: "pages#experience")
   get("/aboutme", to: "pages#aboutme")
 
+  get("/secret_login", to: "sessions#new")
+  post("/secret_login", to: "sessions#create")
+  delete("/secret_logout", to: "sessions#destroy")
+
+  resources(:users)
+
 end
